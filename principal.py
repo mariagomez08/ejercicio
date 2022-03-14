beb9868edb284f9f84875ea2515c149b942a344a
María Gómez <88645665+mariagomez08@users.noreply.github.com>
2022-03-14 14:13:05 -0500

    Area esfera

--- principal.py
+++ principal.py
@@ -26,3 +26,10 @@ n3 = float( input("Ingreasa medida del lado c: " ))
 
 res = Volumen_del_paralelepipedo(n1,n2,n3)
 print(f"El volumen es {res}")
+
+def area_esfera(radio):
+    area=4*3.1415*(radio*radio)
+    return print(f"el area de la esfera es {area}")
+
+radio=int(input("ingrese el radio:" ))
+area= area_esfera(radio)
