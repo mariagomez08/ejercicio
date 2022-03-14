1d40d100237cb0852c4b320eacbea925c783de69
María Gómez <88645665+mariagomez08@users.noreply.github.com>
2022-03-14 14:08:24 -0500

    Area esfera

--- principal.py
+++ principal.py
@@ -8,4 +8,11 @@ def rhombusArea(d1,d2):
     return area
 	
 area_cuadrado(6) 
-	
+
+
+def area_esfera(radio):
+    area=4*3.1415*(radio*radio)
+    return print(f"el area de la esfera es {area}")
+
+radio=int(input("ingrese el radio:" ))
+area= area_esfera(radio)
